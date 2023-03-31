3b4159de2111a0bad8032e06056c0bb7d1d29edd
Marjorie <Marjorie@Marjorie>
2023-03-31 20:50:41 +0400

    Homework 2 First commit

---
+++ MotorPhEmployeeApp/src/MotorPhEmployeeApp/Employee.java
@@ -0,0 +1,46 @@
+package MotorPhEmployeeApp;
+
+public class Employee {
+	public static void main(String[] args) {
+		
+	       
+	}
+	
+	        public int employeeNumber;
+	        public String name;
+	        public String address;
+	        public String jobTitle;	
+	
+	
+	        public void employee (int employeeNumber, String name, String address,String jobTitle) {
+	               this.employeeNumber = employeeNumber;
+	               this.name = name;
+	               this.address = address;
+	               this.jobTitle = jobTitle; 
+              
+	        }
+	        
+            public int getemployeeNumber() {
+            	   return employeeNumber; 
+            }
+	        
+            public String getname() {
+            	   return name;
+            	   
+            }
+            
+            public String getaddress() {
+            	   return address;
+            	   
+            }
+            
+            public String getjobTitle() {
+            	   return jobTitle; 
+            }
+            
+            
+ }
+
+
+
+
