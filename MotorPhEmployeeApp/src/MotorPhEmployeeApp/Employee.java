5aa121dcd7a73f0205128ddaa672056e6068d366
Marjorie <Marjorie@Marjorie>
2023-04-01 17:46:46 +0400

    Home Work 2 first commit

--- MotorPhEmployeeApp/src/MotorPhEmployeeApp/Employee.java
+++ MotorPhEmployeeApp/src/MotorPhEmployeeApp/Employee.java
@@ -39,7 +39,81 @@ public class Employee {
             }
             
             
- }
+            
+            public String Attendance;
+                   int daysPresent;
+                   int daysAbsent;
+            
+            public void Attendance (int daysPresent, int daysAbsent) {
+            	   this.daysAbsent = daysPresent;
+            	   this.daysAbsent =  daysAbsent;
+            	   
+            }
+            
+            public int getdaysPresent() {
+            	   return daysPresent;
+            } 	   
+           
+            public int getdaysAbsent() {
+            	   return daysAbsent;
+            }
+                   
+            
+            
+            public String Benefits; 
+                    int RiceSubcidy;
+                    int PhoneAllowance;
+                    int ClothingAllowance;
+                    
+            public  void Benefits (int RiceSubcidy, int PhoneAllowance, int ClothingAllowance) {  
+            	    this.RiceSubcidy = RiceSubcidy;
+            	    this.PhoneAllowance = PhoneAllowance;
+            	    this.ClothingAllowance = ClothingAllowance;
+            	    
+             }
+            public int getRiceSubcidy() {
+                   return RiceSubcidy;
+             }       
+            public int getPhoneAllowance() {
+            	   return PhoneAllowance;
+            
+            }
+            public int getClothingAllowance() {
+            	   return ClothingAllowance;
+            }
+            
+            public int Deduction;
+                   
+                   double SSS;
+                   double PhilHealth;
+                   double Pagibig;
+                   double Tax;
+                   
+                   
+             public void Deduction (double SSS, double PhilHealth, double Pagibig, double Tax) {
+            	   this.SSS = SSS;
+            	   this.PhilHealth = PhilHealth;
+            	   this.Pagibig = Pagibig;
+            	   this.Tax = Tax;
+            	   
+             }
+             public double getSSS() {
+            	    return SSS;
+            	   
+ 
+             }
+             public double getPhilHealth () {
+            	    return PhilHealth;
+            	   
+             }
+             public double getPagibig () {
+            	    return Pagibig;
+             }
+             public double getTax () {
+            	    return Tax;
+             }
+            
+   }
 
 
 
